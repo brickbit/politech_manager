cc366982d44709e61bf4b25cbef4ae6645eeb5a9
Roberto <92rober@gmail.com>
2022-05-21 11:30:12 +0200

    fix: minor fix in loginScreen UI

--- lib/app/views/screens/login_screen.dart
+++ lib/app/views/screens/login_screen.dart
@@ -65,69 +65,65 @@ class LoginScreen extends GetView<LoginController> {
             ),
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ColorFiltered(
-                      colorFilter: ColorFilter.mode(
-                          (Colors.grey[400])!, BlendMode.modulate),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                          image: AssetImage('assets/images/mural_epcc.png'),
-                          fit: BoxFit.cover,
-                        )),
-                        height: mobile ? 280 : 320,
-                      ),
+              child: Column(
+                children: [
+                  ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        (Colors.grey[400])!, BlendMode.modulate),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage('assets/images/mural_epcc.png'),
+                        fit: BoxFit.cover,
+                      )),
+                      height: mobile ? 250 : 320,
                     ),
-                    SizedBox(
-                        width: userTextFieldWidth,
-                        child: TextField(
-                          controller: _usernameController,
-                          decoration: InputDecoration(labelText: 'email'.tr),
-                        )),
-                    const SizedBox(
-                      height: 16.0,
+                  ),
+                  SizedBox(
+                      width: userTextFieldWidth,
+                      child: TextField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(labelText: 'email'.tr),
+                      )),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  SizedBox(
+                    width: passwordTextFieldWidth,
+                    child: PasswordTextField(
+                      title: 'password'.tr,
+                      passwordController: _passwordController,
                     ),
-                    SizedBox(
-                      width: passwordTextFieldWidth,
-                      child: PasswordTextField(
-                        title: 'password'.tr,
-                        passwordController: _passwordController,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    SizedBox(
-                        width: buttonWidth,
-                        height: 45,
-                        child: ElevatedButton(
-                            child: Text('login'.tr),
-                            onPressed: () {
-                              controller.login(_usernameController.text,
-                                  _passwordController.text);
-                            })),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          Get.toNamed(Routes.recoverPassword);
-                        },
-                        child: Text('forgottenPassword'.tr)),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          Get.toNamed(Routes.register);
-                        },
-                        child: Text('register'.tr)),
-                  ],
-                ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  SizedBox(
+                      width: buttonWidth,
+                      height: 45,
+                      child: ElevatedButton(
+                          child: Text('login'.tr),
+                          onPressed: () {
+                            controller.login(_usernameController.text,
+                                _passwordController.text);
+                          })),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Get.toNamed(Routes.recoverPassword);
+                      },
+                      child: Text('forgottenPassword'.tr)),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Get.toNamed(Routes.register);
+                      },
+                      child: Text('register'.tr)),
+                ],
               ),
             ),
           ),
